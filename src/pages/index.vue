cc7db4fef301d1322745c5d5ee0e5b0efc88b988
Manoah <149895ja@gmail.com>
2023-10-09 22:04:25 +0200

    chore: show variable icon bug

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
     <p>Hello world</p>
+    <v-btn>hardcoded <v-icon icon="i-mdi-home" /></v-btn>
+    <v-btn @click="condition = !condition">conditional <v-icon :icon="'i-mdi-tv' + (condition ? '-off' : '')" /></v-btn>
+    <v-btn @click="condition = !condition">variable string<v-icon :icon="`i-mdi-account${condition ? '' : '-circle'}`"/></v-btn>
   </div>
 </template>
+<script setup lang="ts">
+const condition = ref(false)
+
+</script>
